d591fe467362d8a30b7f26e826f270e75e4dfd92
adityaKumar <adityakumarrajput01@gmail.com>
2024-02-29 14:41:03 +0530

    setting up frontend and chakra ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@chakra-ui/react"
+
+
+function App() {
+
+  return (
+    <>
+      hello
+      <Button colorScheme='blue'>Button</Button>
+    </>
+  )
+}
+
+export default App
