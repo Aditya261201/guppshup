79d064483dc11dda231e06d701127defc7013c28
adityaKumar <adityakumarrajput01@gmail.com>
2024-03-02 14:32:19 +0530

    API'S  fetch user chats,create group ,rename group, add user,remove user

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,17 +1,17 @@
 import express from "express"
 import { protect } from "../middlewares/authMiddleware.js";
-import {accessChats} from "../controllers/chat.js"
+import { accessChats, fetchChats, createGroupChat, renameGroup, removeFromGroup , addToGroup } from "../controllers/chat.js"
 
 
 const router = express.Router();
 
 
-// router.get('/',protect,fetchChats);
+router.get('/',protect,fetchChats);
 router.post('/',protect,accessChats);
-// router.post('/group',protect,createGroup);
-// router.put('/rename',protect,renameGroup);
-// router.put('/removegroup',protect,removeFromGroup);
-// router.put('/groupadd',protect,addToGroup);
+router.post('/group',protect,createGroupChat);
+router.put('/rename',protect,renameGroup);
+router.put('/removegroup',protect,removeFromGroup);
+router.put('/groupadd',protect,addToGroup);
 
 
 export default router;
